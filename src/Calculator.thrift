5d6f0fa66ed564bf228866020f64638c0f9cb036
Sachith Withana <swsachith@gmail.com>
2014-02-08 14:26:40 -0500

    initial commit

---
+++ src/Calculator.thrift
@@ -0,0 +1,10 @@
+namespace php Calculator
+namespace java lk.swithana.calculator
+ 
+service CalculatorService
+{
+    i32 add  (1:i32 a, 2:i32 b),
+    i32 sub  (1:i32 a, 2:i32 b),
+    i32 div  (1:i32 a, 2:i32 b),
+    i32 mult (1:i32 a, 2:i32 b)
+}
